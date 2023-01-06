19e6ac195faa1314cf7485619849a050ad802a2d
Ren <renperez157@gmail.com>
2023-01-05 23:09:48 -0500

    first commit

--- pages/_document.js
+++ pages/_document.js
@@ -1,13 +1,29 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import Document from "next/document";
+import { ServerStyleSheet } from "styled-components";
 
-export default function Document() {
-  return (
-    <Html lang="en">
-      <Head />
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  )
+export default class MyDocumnet extends Document {
+  static async getInitialProps(ctx) {
+    const sheet = new ServerStyleSheet();
+    const originalRenderPage = ctx.rendePage;
+    try {
+      ctx.rendePage = () =>
+        originalRenderPage({
+          enhanceApp: (App) => (props) =>
+            sheet.collectStyles(<App {...props} />),
+        });
+      const initialProps = await Document.getInitialProps(ctx);
+
+      return {
+        ...initialProps,
+        styles: (
+          <>
+            {initialProps.styles}
+            {sheet.getStyleElement()}
+          </>
+        ),
+      };
+    } finally {
+      sheet.seal();
+    }
+  }
 }
---
+++ pages/pokemones/[id].js
@@ -0,0 +1,25 @@
+import Image from "next/image";
+import Link from "next/link";
+
+const Pokemon = ({ data }) => {
+  console.log(data);
+  return (
+    <div>
+      <h1>
+        {data.name} N°{data.id}
+      </h1>
+      <Image src={data.sprites.front_default} width={400} height={400} alt={data.name}></Image>
+      <Link href="/"> Back</Link>
+    </div>
+  );
+};
+
+export default Pokemon;
+
+export const getServerSideProps = async ({ params }) => {
+  const response = await fetch(
+    `https://pokeapi.co/api/v2/pokemon/${params.id}`
+  );
+  const data = await response.json();
+  return { props: { data } };
+};
